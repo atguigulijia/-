27822ad0ff31a216d7411c6b0f3f97056da83d25
lijia <lijia@qq.com>
2022-12-27 14:34:52 +0800

    加入redis配置

---
+++ src/main/java/com/itheima/reggie/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.itheima.reggie.config;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author lijia
+ * @create 2022-12-27 14:23
+ */
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        //默认序列化器keySerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+
+
+}
