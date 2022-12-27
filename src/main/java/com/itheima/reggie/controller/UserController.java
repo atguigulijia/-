124c5531fdd0f25ef0a8eb63690ab5fd9623d87b
lijia <lijia@qq.com>
2022-12-27 14:58:11 +0800

    对短信验证码进行了缓存

--- src/main/java/com/itheima/reggie/controller/UserController.java
+++ src/main/java/com/itheima/reggie/controller/UserController.java
@@ -6,10 +6,12 @@ import com.itheima.reggie.service.UserService;
 import com.itheima.reggie.util.EmailUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author lijia
@@ -22,14 +24,19 @@ import java.util.Map;
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
     @PostMapping("/login")
     public R<User> login(@RequestBody Map<String, String> map, HttpSession session) {
-        String regVerifiCode = (String) session.getAttribute("regVerifiCode");
-        if (StringUtils.isEmpty(regVerifiCode)) return R.error("验证码已失效，请重新获取验证码");
         String phone = map.get("phone");
         String code = map.get("code");
-        if (StringUtils.isEmpty(phone)) return R.error("邮箱号码不能为空");
-        if (StringUtils.isEmpty(code)) return R.error("验证码不能为空");
+//        String regVerifiCode = (String) session.getAttribute("regVerifiCode");
+        //从redis中获取对应手机的验证码
+        String regVerifiCode = (String) redisTemplate.opsForValue().get(phone);
+        if (StringUtils.isEmpty(regVerifiCode)) return R.error("验证码已失效，请重新获取验证码");
+
+
         //进行验证码比对
         if (regVerifiCode.equalsIgnoreCase(code)){
             //判断是否为新用户，新用户直接注册并登录
@@ -40,16 +47,20 @@ public class UserController {
                 user.setPhone(phone);
                 user.setStatus(1);
                 userService.save(user);
-                session.removeAttribute("regVerifiCode");   //登录后验证码失效
+//                session.removeAttribute("regVerifiCode");   //登录后验证码失效
                 session.setAttribute("user",user.getId());
+                //清除验证码
+                redisTemplate.delete(phone);
                 return R.success(user);
             }
             //老用户
             if (user.getStatus() ==0){
                 return R.error("账号已禁用");
             }
-            session.removeAttribute("regVerifiCode");
-            session.setAttribute("user",user.getId()); //登录后验证码失效
+//            session.removeAttribute("regVerifiCode"); //登录后验证码失效
+            session.setAttribute("user",user.getId());
+            //清除验证码
+            redisTemplate.delete(phone);
             return R.success(user);
         }
         return R.error("验证码错误，登录失败");
@@ -61,8 +72,11 @@ public class UserController {
         if (!StringUtils.isEmpty(phone)){
                 String regVerifiCode = String.valueOf(EmailUtil.sendEmailRegCode(phone));
                 log.info("验证码为"+regVerifiCode);
-                session.setAttribute("regVerifiCode",regVerifiCode);
-                session.setMaxInactiveInterval(3 *60);
+//                session.setAttribute("regVerifiCode",regVerifiCode);
+//                session.setMaxInactiveInterval(3 *60);
+
+                //将验证码缓存在redis中  key为手机号
+                redisTemplate.opsForValue().set(phone,regVerifiCode,3, TimeUnit.MINUTES);
                 return R.success("邮箱验证码发送成功");
         }
         return R.error("邮箱号码不能为空");
